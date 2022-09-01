dae277d4247a3675f50bad693bf518597a90a241
Victoria Beasley <iesnoth91@gmail.com>
2022-08-31 19:51:14 -0500

    trying to understand collision

--- index.js
+++ index.js
@@ -5,25 +5,42 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-function looping (){
+function looping() {
     npc.walkNorth(1400, () => {
-    npc.walkEast(1200, () => {
-        npc.walkSouth(300, () =>{
-            npc.walkEast(1500, () =>{
-                npc.walkSouth(1500, () => {
-                    npc.walkWest(2700,() => {
-                        npc.walkNorth(400,() =>{
-                            looping()
+        npc.walkEast(1200, () => {
+            npc.walkSouth(300, () => {
+                npc.walkEast(1500, () => {
+                    npc.walkSouth(1500, () => {
+                        npc.walkWest(2700, () => {
+                            npc.walkNorth(400, () => {
+                                looping()
+                            })
                         })
                     })
                 })
             })
         })
     })
-})
 }
 
 looping()
+
+//https://learnopengl.com/In-Practice/2D-Game/Collisions/Collision-detection
+
+// function CheckCollision() { // AABB - AABB collision
+//     {
+//     // collision x-axis?
+//     let collisionX = npc.Position.x + npc.Size.x >= pc.Position.x &&
+//             pc.Position.x + pc.Size.x >= npc.Position.x;
+//     // collision y-axis?
+//     let collisionY = npc.Position.y + npc.Size.y >= pc.Position.y &&
+//             pc.Position.y + pc.Size.y >= npc.Position.y;
+//         // collision only if on both axes
+//         return collisionX && collisionY;
+//     }
+// }
+
+// CheckCollision()
 
 // Create the inventory
 const inventory = newInventory()
